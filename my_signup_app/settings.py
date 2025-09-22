3dc75cc50176c3417d4a361cf5d81e1a9fe6efbd
byagashaniaustine <byagashaniaustine@gmail.com>
2025-09-22 12:58:36 +0300

    fixes

--- my_signup_app/settings.py
+++ my_signup_app/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -----------------------------
 # SECURITY
 # -----------------------------
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+DJANGO_SECRET_KEY= os.environ.get("DJANGO_SECRET_KEY")
 
 print("DJANGO_SECRET_KEY:", os.environ.get("DJANGO_SECRET_KEY"))
 
